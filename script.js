1e3cdc0cda720e061a0f0e54c381bc979384b9ee
Chaitanya Prabuddha <chaitanyaplusplus@gmail.com>
2025-01-31 20:37:59 +0530

    third commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const inputBox = document.getElementById("input-box");
+
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("write something here");
+    }
+    
+    else{
+        let li = document.createElement("li");
+        li innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = documentcreateElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+    }
+
+    inputBoxl.value = "";
+    saveData();
+
+}
+
+listContainer.addEventListener("click", function(e)) {
+    if (e.target.tagName === "LI") {
+        e.target.classlist.toogle("checked");
+        saveData();
+
+    }
+    
+    else if(e.target.tagName === "SPAN");
+    e.target.parentElement.remove();
+    saveData();
+
+
+}, false);
+
+function saveData() {
+    localStorage.setItem("Data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.ineerHTML = localStorage.getItem("data");
+}
+
+showTask();
